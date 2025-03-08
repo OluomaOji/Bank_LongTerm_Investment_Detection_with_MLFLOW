90185042b82393e6027e5d37dbf536ef8b6bb988
OluomaOji <Oluomaoji@gmail.com>
2025-03-08 11:23:44 +0000

    Logging and exception

---
+++ src/logging.py
@@ -0,0 +1,20 @@
+import logging
+import os
+
+# set directory
+Log_dir = "Logs"
+if not os.path.exists(Log_dir):
+    os.makedirs(Log_dir)
+
+# Set the Logging Configuration
+logging.basicConfig(
+    level= logging.INFO,
+    format='%(asctime)s-%(name)s-%(levelname)s-%(message)s',
+    handlers=[
+        logging.FileHandler(os.path.join(Log_dir,"bank_loan_detector.logs")),
+        logging.StreamHandler()
+    ]
+)
+
+def get_logger(name):
+    return logging.getLogger(name)
